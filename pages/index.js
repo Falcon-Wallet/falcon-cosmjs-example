db68b4c74efe342e0020e0a26e89102b02e2dfe3
besated <bryan@falconwallet.app>
2022-06-21 14:58:27 -0700

    Add CosmJS example

--- pages/index.js
+++ pages/index.js
@@ -1,192 +1,111 @@
+import { useState } from 'react'
 import Head from 'next/head'
+import { assertIsBroadcastTxSuccess, SigningStargateClient } from "@cosmjs/stargate";
 
 export default function Home() {
+  const [connected, setConnected] = useState(false);
+  const [address, setAddress] = useState('');
+  const [toAddress, setToAddress] = useState('')
+  const [amount, setAmount] = useState('');
+
+  const chainId = 'osmosis-1';
+  const rpcUrl = 'https://rpc-osmosis.blockapsis.com';
+
+  const connect = async () => {
+    if (!window.falcon) {
+      alert('Please install the Falcon Wallet extension');
+    }
+    try {
+      await window.falcon.connect();
+      setConnected(true);
+
+      // Get address
+      const account = await window.falcon.getAccount(chainId);
+      setAddress(account.address);
+    } catch (e) {
+      // User declined connection
+      console.log(e);
+    }
+  }
+
+  const send = async (e) => {
+    e.preventDefault();
+    if (!toAddress || !amount) {
+      return;
+    }
+
+    // Getting offline signer
+    const offlineSigner = await window.falcon.getOfflineSigner(chainId);
+    const [account] = await offlineSigner.getAccounts();
+
+    // Create client using signer
+    const client = await SigningStargateClient.connectWithSigner(
+      rpcUrl,
+      offlineSigner
+    )
+
+    const parsedAmount = parseFloat(amount) * Math.pow(10, 6);
+    const coin = {
+      denom: 'uosmo',
+      amount: parsedAmount.toString(),
+    }
+    const fee = {
+      amount: [{
+        denom: 'uosmo',
+        amount: '5000',
+      },],
+      gas: '200000',
+    }
+    const result = await client.sendTokens(account.address, toAddress, [coin], fee, "Send tokens");
+    assertIsBroadcastTxSuccess(result);
+  }
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Falcon CosmJs Example</title>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className="header">Falcon CosmJS Example</h1>
+        {connected ? (
+          <div>
+            <p>Chain ID: {chainId}</p>
+            <p>Address: {address}</p>
+            <h2 className="header">Send</h2>
+            <form onSubmit={send}>
+              <div className="form-group">
+                <label htmlFor="recipient">Recipient: </label>
+                <input id="recipient" type="text" onChange={(e) => setToAddress(e.target.value)} value={toAddress} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="amount">Amount (OSMO): </label>
+                <input id="amount" type="text" onChange={(e) => setAmount(e.target.value)} value={amount} />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        ) : (
+          <div className="header">
+            <button onClick={connect}>Connect</button>
+          </div>
+        )}
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
         .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+          padding: 1em;
+          max-width: 600px;
+          width: 100vw;
+          margin: 0 auto;
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .form-group {
+          margin-bottom: 1em;
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+        
+        .header {
           text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+          margin-bottom: 1em;
         }
       `}</style>
 
